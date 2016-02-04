f2449203a73c0b4443f092e9f2490f34700ef090
Tobias Bradtke <webwurst@gmail.com>
2016-02-04 12:47:26 +0100

    Let's try to publish during build

---
+++ master/publish.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+docker login \
+  --unsername $$DOCKER_USER \
+  --password $$DOCKER_PASSWORD \
+  --email $$DOCKER_EMAIL
+
+COMPONENT=weave
+docker build \
+  --tag armhfbuild/$COMPONENT:$VERSION \
+  weave/prog/$COMPONENT
+docker push armhfbuild/$COMPONENT:$VERSION
+
+COMPONENT=weaveexec
+docker build \
+  --tag armhfbuild/$COMPONENT:$VERSION \
+  weave/prog/$COMPONENT
+docker push armhfbuild/$COMPONENT:$VERSION
+
+COMPONENT=plugin
+docker build \
+  --tag armhfbuild/weaveplugin:$VERSION \
+  weave/prog/$COMPONENT
+docker push armhfbuild/weaveplugin:$VERSION
+
+
+# ensure prefix 'weave'
+# weave${COMPONENT##weave}
